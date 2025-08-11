31ea026ed8be5c9860e45a8c9ee6e56c04673c68
Vamsi <devopsaws925@gmail.com>
2025-08-11 04:36:26 +0000

    Added app.js file

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Define the hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  // Set response headers
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Respond with message
+  res.end('Hello, World!\n');
+});
+
+// Start the server
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
